fbaa58f7db71d34eee58c4c89dcd15f5a0e7e952
hyojhand <sonjw5128@gmail.com>
2022-04-15 00:08:33 +0900

    spring-db JDBC

---
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,43 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException {
+        //save
+        Member member = new Member("memberV10", 10000);
+        repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember); // 롬복의 toString때문에 참조값이 아닌 toString으로 출력된다.
+        log.info("member == findMember {}", member == findMember); // member 와 findMember는 다른객체이다.
+        log.info("member equals findMember {}", member.equals(findMember)); // 롬복의 equals AndHashcode 로 equals는 값이 같으니 true 출력
+        assertThat(findMember).isEqualTo(member);
+
+        //update : money 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        // delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+
+    }
+}
